f4699cd868df33b6cff86a374a1a78d265a96ce7
Vikrant <vikrantmom5748@gmail.com>
2025-12-31 12:47:42 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen'>
+        {/* Theme Toggle */}
+
+      {/* Background Effects */}
+
+      {/* Navbar  */}
+
+      {/* Main content  */}
+
+      {/* Footer  */}
+
+    </div>
+  )
+}
+
+export default Home
